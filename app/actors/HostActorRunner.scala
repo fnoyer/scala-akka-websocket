789b712a3d4e4b3a53dacaea63290846e1e31541
fnoyer <fredericnoyer.nyc@gmail.com>
2017-08-08 20:36:25 +0200

    Some cleanUP var--> val etc..

--- app/actors/HostActorRunner.scala
+++ app/actors/HostActorRunner.scala
@@ -11,9 +11,9 @@ import scala.language.postfixOps
 
 class HostActorRunner(profileService: ProfileService) {
 
-  implicit val system: ActorSystem = ActorSystem("ActorSystem")
+  val system: ActorSystem = ActorSystem("ActorSystem")
   implicit val executionContext = system.dispatcher
-  var hostActorRef = system.actorOf(Props(new HostActor(profileService)), "hostActor")
+  private val hostActorRef = system.actorOf(Props(new HostActor(profileService)), "hostActor")
 
   system.scheduler.schedule(0.microseconds, 5.seconds) {
    hostActorRef  ! Tick
